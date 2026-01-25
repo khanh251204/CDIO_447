cdda79201429f70444c411d9de93194b95a878b1
khanh251204 <hoangkhanh281004@gmail.com>
2026-01-25 10:20:22 +0700

    nhu_header

--- src/Component/Layouts/Header.jsx
+++ src/Component/Layouts/Header.jsx
@@ -4,34 +4,29 @@ import Button from "../UI/Button"
 import { useNavigate } from "react-router-dom"
 
 export const Header = () => {
-  const navigate = useNavigate()
-  return (
-    <nav>
-      <div className={styles.container}>
-        <a href="/">
-          <img
-            className={styles.logo}
-            src={logo}
-          />
-        </a>
-        <div className={styles.btn}>
-          <Button
-            onClick={() => navigate("/Login")}
-            text="Đăng Nhập"
-            className={styles.btnLogin}
-          />
-          <Button
-            onClick={() => navigate("/Register")}
-            text="Đăng Ký"
-            className={styles.btnRegister}
-          />
-        </div>
-
-
-
-
-
-      </div>
-    </nav>
-  );
+    const navigate = useNavigate()
+    return (
+        <nav>
+            <div className={styles.container}>
+                <a href="/">
+                    <img
+                        className={styles.logo}
+                        src={logo}
+                    />
+                </a>
+                <div className={styles.btn}>
+                    <Button
+                        onClick={() => navigate("/Login")}
+                        text="Đăng Nhập"
+                        className={styles.btnLogin}
+                    />
+                    <Button
+                        onClick={() => navigate("/Register")}
+                        text="Đăng Ký"
+                        className={styles.btnRegister}
+                    />
+                </div>
+            </div>
+        </nav>
+    );
 };
